1bce15463baa355613de010b33546eb3a661d768
vel <vmv2k05@gmail.com>
2025-02-27 20:14:46 +0530

    modified

--- server/server.py
+++ server/server.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # To handle CORS easily
 import util
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
 
-@app.route('/get_location_names', methods=['GET'])
+@app.route('/api/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@app.route('/api/predict_home_price', methods=['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
@@ -22,11 +22,9 @@ def predict_home_price():
     response = jsonify({
         'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run()
+    app.run(debug=True)
